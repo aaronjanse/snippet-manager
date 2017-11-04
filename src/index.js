366b85a2a14beaf585ce4e47b271cd0c7ca7f767
Aaronduino <aaron.janse@gmail.com>
2017-11-04 11:47:56 -0700

    experimental auto-updating

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
 'use strict';
 const electron = require('electron');
+const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
+
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  mainWindow.webContents.send('message', text);
+}
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 var fs = require('fs');
-
-const app = electron.app;
 
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
@@ -43,6 +75,7 @@ app.on('activate', () => {
 
 app.on('ready', () => {
 	mainWindow = createMainWindow();
+	autoUpdater.checkForUpdatesAndNotify();
 });
 
 const ipc = require('electron').ipcMain;
